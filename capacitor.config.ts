13d6831ad9cd4af4713016ddfcae87d5dccf3341
roman_turner <rn.turner@live.com>
2022-02-02 13:29:06 -0800

    QR Code Scanner debug

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'qr.scanner.test',
+  appName: 'qr_scanner',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
